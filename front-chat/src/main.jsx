db2fe34e3dec73c53ee3fb12aa1cc93b3c2efb53
Divash230 <divash941@gmail.com>
2025-10-15 01:24:38 +0530

    Fix import path casing in main.jsx

--- front-chat/src/main.jsx
+++ front-chat/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './config/routes.jsx';
+import AppRoutes from './config/Routes.jsx';
 import { Toaster } from 'react-hot-toast';
 import { ChatProvider } from './context/ChatContext.jsx';
 
