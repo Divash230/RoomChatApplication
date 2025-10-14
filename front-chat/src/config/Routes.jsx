5563933982526cf72bf4f2d43dc504716efb4fa6
Divash230 <divash941@gmail.com>
2025-10-15 01:20:44 +0530

    Fix: corrected Routes.jsx element prop typo

--- front-chat/src/config/Routes.jsx
+++ front-chat/src/config/Routes.jsx
@@ -9,7 +9,7 @@ const AppRoutes = () => {
         <Route path="/" element={<App />} />
         <Route path="/chat" element={<h1><ChatPage/></h1>} />
         <Route path="/about"element={<h1>This is about page</h1>} />
-        <Route path="*" elements={<h1>404 Page Not Found</h1>} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
     </Routes>
   );
   
